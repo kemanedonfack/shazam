a32d2f46871288fb1c8eaf557281e5208cc8a8f0
Kemane Donfack <kemanedonfack5@gmail.com>
2022-09-17 16:35:38 +0200

    creation du model

---
+++ lib/models/data.dart
@@ -0,0 +1,5 @@
+import 'music.dart';
+
+List<Music> listmusics = [
+  Music(id: 1, artiste: 'Imagine Dragons', image: 'assets/images/badliar.jpg', nom: 'Bad Liar'),
+];
---
+++ lib/models/music.dart
@@ -0,0 +1,10 @@
+class Music{
+  
+  int id;
+  String nom;
+  String image;
+  String artiste;
+
+  Music({ required this.id,required this.nom, required this.image, required this.artiste});
+
+}
